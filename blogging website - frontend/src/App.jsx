ba58e10d01365cce60a4b06c8e49db46484ca078
MAC 2016 <sobanatban4@gmail.com>
2024-11-14 17:30:24 +0500

    Updated .gitignore and made frontend updates

--- blogging website - frontend/src/App.jsx
+++ blogging website - frontend/src/App.jsx
@@ -1,6 +1,8 @@
+import NavbarComponent from "./components/NavbarComponent";
+
 const App = () => {
     return (
-        <h1>MERN Blogging website by modern web</h1>
+        <NavbarComponent />
     )
 }
 
